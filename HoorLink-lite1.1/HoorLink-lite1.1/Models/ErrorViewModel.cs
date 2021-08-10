f50d86bcec4857630c9e046a98d7473350fdc660
Ahmed Nasr <anasr3883@gmail.com>
2021-08-10 04:35:07 +0200

    initial

---
+++ HoorLink-lite1.1/HoorLink-lite1.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HoorLink_lite1._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
